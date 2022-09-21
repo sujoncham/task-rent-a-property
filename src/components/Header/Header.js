6c0cf6ce2091db2ba2f591f071bcdefc2046bf4e
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-21 06:24:35 +0600

    created necessary components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Estatery</h1>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
